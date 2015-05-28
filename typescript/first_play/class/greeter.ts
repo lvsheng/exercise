f69bdf0aa05b28d26f71fa2a9e1eb6aa3ecd22cd
lvsheng <lvsheng@baidu.com>
2015-05-29 01:34:16 +0800

    for save

---
+++ typescript/first_play/class/greeter.ts
@@ -0,0 +1,19 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+class Student {
+    fullName: string;
+    constructor (public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+function greeter (person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
---
+++ typescript/first_play/type_detect/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+document.body.innerHTML = greeter("string");
+//# sourceMappingURL=greeter.js.map

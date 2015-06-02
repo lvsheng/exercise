cd6ecccbff3c27e979185b82693044143d66bff3
lvsheng <lvsheng@baidu.com>
2015-06-02 13:31:20 +0800

    limit the code line amount of hello-world to 27 line

--- angular2/share/hello-world/app.ts
+++ angular2/share/hello-world/app.ts
@@ -1,8 +1,12 @@
 /// <reference path="../../lib/typings/angular2/angular2.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
 
-@Component({ selector: 'my-app' })
-@View({ template: '<h1>Hello, {{ name }}!</h1>' })
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1>Hello, {{ name }}!</h1>'
+})
 class MyAppComponent {
     name: string;
     constructor () {

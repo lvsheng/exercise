ae28dbc40d342c7f0587d80e49242171363c0683
lvsheng <lvsheng@baidu.com>
2015-06-03 04:22:10 +0800

    outer change detect for angular2

--- angular2/guide-exercise/displaying-data/app.ts
+++ angular2/guide-exercise/displaying-data/app.ts
@@ -5,12 +5,20 @@ import {Component, View, bootstrap} from 'angular2/angular2';
     selector: 'my-app'
 })
 @View({
-    template: '<h1>Hello, {{ name }}!</h1>'
+    template: `
+    <h1>Hello, {{ name }}!</h1>
+    `
 })
 class MyAppComponent {
     name: string;
     constructor () {
         this.name = 'tb_fe';
+
+        //testing for outer change
+        setInterval(function () {
+            debugger;
+            this.name = 'tb_fe second: ' + (new Date().getSeconds());
+        }.bind(this), 1000);
     }
 }
 

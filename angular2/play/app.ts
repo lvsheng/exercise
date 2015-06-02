879c5be6a31a641e76f9db10b2f43f60f5c6cc3e
lvsheng <lvsheng@baidu.com>
2015-06-02 13:03:54 +0800

    remove .idea

--- angular2/play/app.ts
+++ angular2/play/app.ts
@@ -1,4 +1,4 @@
-/// <reference path="lib/typings/angular2/angular2.d.ts" />
+/// <reference path="../lib/typings/angular2/angular2.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
 
 @Component({

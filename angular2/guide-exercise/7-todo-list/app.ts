c1d8ffb593d06cb9fd4742dac8f8285ab978a431
lvsheng <lvsheng@baidu.com>
2015-06-03 16:08:06 +0800

    copy 7 from 6

---
+++ angular2/guide-exercise/7-todo-list/app.ts
@@ -0,0 +1,53 @@
+/// <reference path="../../lib/typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap, For, If} from 'angular2/angular2';
+
+class FriendsService {
+    names: Array<string>;
+    constructor () {
+        this.names = ['tb_qa', 'tb_pm', 'tb_rd', 'baidu'];
+    }
+}
+
+@Component({
+    selector: 'my-app',
+    injectables: [FriendsService]
+})
+@View({
+    template: `
+    <h1>Hello, {{ name }}!</h1>
+    <h2>our friends:</h2>
+    <ul>
+        <li *for="#friend of friends">
+            {{ friend }}
+        </li>
+    </ul>
+    <p *if="friends.length > 3">
+        so many~
+    </p>
+    <input type="text" #myname/>
+    <p>{{ myname }}</p>
+    <p>{{ myname.value }}</p>
+    `,
+    directives: [For, If]
+})
+class MyAppComponent {
+    name: string;
+    friends: Array<string>;
+
+    constructor (friendsService: FriendsService) {
+        this.name = 'tb_fe';
+        this.friends = friendsService.names;
+
+        //testing for outer change
+        setInterval(function () {
+            this.name = 'tb_fe second: ' + (new Date().getSeconds());
+        }.bind(this), 1000);
+    }
+
+    sayHello (myname) {
+        console.log('hello');
+        console.log(myname);
+    }
+}
+
+bootstrap(MyAppComponent);

9d64b3b81954b8cccbc42a6681144a218f260230
lvsheng <lvsheng@baidu.com>
2015-06-03 14:52:53 +0800

    copy 6 from 5 and 4 (merge)

---
+++ angular2/guide-exercise/6-event/app.ts
@@ -0,0 +1,45 @@
+/// <reference path="../../lib/typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap, For, If} from 'angular2/angular2';
+
+class FriendsService {
+    names: Array<string>;
+    constructor () {
+        this.names = ['tb_qa', 'tb_pm', 'tb_rd', 'baidu'];
+    }
+}
+
+@Component({
+    selector: 'my-app',
+    injectables: [FriendsService]
+})
+@View({
+    template: `
+    <h1>Hello, {{ name }}!</h1>
+    <h2>our friends:</h2>
+    <ul>
+        <li *for="#friend of friends">
+            {{ friend }}
+        </li>
+    </ul>
+    <p *if="friends.length > 3">
+        so many~
+    </p>
+    `,
+    directives: [For, If]
+})
+class MyAppComponent {
+    name: string;
+    friends: Array<string>;
+
+    constructor (friendsService: FriendsService) {
+        this.name = 'tb_fe';
+        this.friends = friendsService.names;
+
+        //testing for outer change
+        setInterval(function () {
+            this.name = 'tb_fe second: ' + (new Date().getSeconds());
+        }.bind(this), 1000);
+    }
+}
+
+bootstrap(MyAppComponent);

ca1d57d39fda399fa94f42e45cb5ffef0816abe6
lvsheng <lvsheng@baidu.com>
2015-06-03 14:03:25 +0800

    copy 5 from 4

---
+++ angular2/guide-exercise/5-service/app.ts
@@ -0,0 +1,34 @@
+/// <reference path="../../lib/typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap, For} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: `
+    <h1>Hello, {{ name }}!</h1>
+    <h2>our friends:</h2>
+    <ul>
+        <li *for="#friend of friends">
+            {{ friend }}
+        </li>
+    </ul>
+    `,
+    directives: [For]
+})
+class MyAppComponent {
+    name: string;
+    friends: Array<string>;
+
+    constructor () {
+        this.name = 'tb_fe';
+        this.friends = ['tb_qa', 'tb_pm', 'tb_rd', 'baidu'];
+
+        //testing for outer change
+        setInterval(function () {
+            this.name = 'tb_fe second: ' + (new Date().getSeconds());
+        }.bind(this), 1000);
+    }
+}
+
+bootstrap(MyAppComponent);
